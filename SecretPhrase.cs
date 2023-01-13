c47ee2c6e8e8c9a8f373023be7d7e39cb4f84fbd
Titanovsky <64440118+Titanovsky@users.noreply.github.com>
2023-01-13 23:01:12 +0500

    [+] Init, error release,
    TODO: Change, when Facepunch will give good chat interface for coding

---
+++ SecretPhrase.cs
@@ -0,0 +1,16 @@
+﻿using Sandbox;
+using System;
+
+namespace Sandbox
+{
+	public partial class SecretPhrase : Sandbox.Game // addon
+	{
+		[ConCmd.Client]
+		static void bloxwich() // lower chars, because this is console command 
+		{
+			Client ply = Local.Client;
+			
+			Log.Info( $"{ply.Name} earned the achievement Secret Phrase" );
+		}
+	}
+}
